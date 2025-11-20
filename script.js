33e0048c9b953915e9fc093f6d6f337a94c4f426
aadi-code <aadikumpavat@gmail.com>
2025-11-20 19:33:55 +0530

    Optimized JS code

--- script.js
+++ script.js
@@ -24,59 +24,73 @@
 const allElem = document.querySelectorAll("*");
 const bgChnage = document.querySelector("button");
 
-const kolors = ["red", "green", "blue", "pink", "black", "white", "yellow", "gold", "purple", "gray"];
+// const kolors = ["red", "green", "blue", "pink", "black", "white", "yellow", "gold", "purple", "gray"];
+const cssColors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
+
+
+
 
 
 
 bgChnage.addEventListener("click", () => {
 
-    let index = kolors[Math.floor(Math.random() * kolors.length)];
-    // console.log(index);
-    
+    let index = cssColors[Math.floor(Math.random() * cssColors.length)];
+
+    // //++++++++++++++++++++++++++++++++This is the First and Unoptimized code +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // const heroSection = document.querySelector("#hero-section");
+    // const extract = window.getComputedStyle(heroSection).backgroundColor;
+    // const rgbStr = extract.slice(4,-1);
+    // const rgbArr = rgbStr.split(",");
+    // const newRgb = rgbArr.map(Number);
+
+    // const R = newRgb[0];
+    // const G = newRgb[1];
+    // const B = newRgb[2];
+
+    // const luminance = 0.2126 * R + 0.7152 * G + 0.0722 * B;
+    // // console.log(luminance);
 
 
+    // // if(luminance > 128){
+    // //     console.log("Light Color");
+    // // }else{
+    // //     console.log("Dark Color");
+    // // }
 
-    // const navLinks = document.querySelector(".nav-links");
+    // allElem.forEach((elem) => {
+    //     elem.style.backgroundColor = index;
 
-const heroSection = document.querySelector("#hero-section");
-
-// const extract = window.getComputedStyle(navLinks).color;
-
-const extract = window.getComputedStyle(heroSection).backgroundColor;
-
-const rgbStr = extract.slice(4,-1);
-
-const rgbArr = rgbStr.split(",");
-
-const newRgb = rgbArr.map(Number);
-
-
-const R = newRgb[0];
-const G = newRgb[1];
-const B = newRgb[2];
+    //     if(luminance < 128){
+    //         elem.style.color = "white";
+    //     }
+    //     else{
+    //         elem.style.color = "black";
+    //     }
+    // })
 
 
-const luminance = 0.2126 * R + 0.7152 * G + 0.0722 * B;
-console.log(luminance);
+    //+++++++++++++++++++++++++++++   This is the Optimized code   +++++++++++++++++++++++++++++++++++++++++++++++++++
 
+    const temp = document.createElement("div");
+    temp.style.color = index;
+    temp.style.display = "none";
+    document.body.appendChild(temp);
 
-// if(luminance > 128){
-//     console.log("Light Color");
-// }else{
-//     console.log("Dark Color");
-// }
+    const rgb = window.getComputedStyle(temp).color;
 
-allElem.forEach((elem) => {
-    elem.style.backgroundColor = index;
+    document.body.removeChild(temp);
+
+    const [R, G, B] = rgb.match(/\d+/g).map(Number);
+
+    const luminance = 0.2126 * R + 0.7152 * G + 0.0722 * B;
 
     if(luminance < 128){
-        elem.style.color = "white";
+        document.body.style.color = "white";
+    }else{
+        document.body.style.color = "black";
     }
-    else{
-        elem.style.color = "black";
-    }
-    })
 
+    document.body.style.backgroundColor = index;
 
 })
 
