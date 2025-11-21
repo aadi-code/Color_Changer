2e4fb2815f5714cd64e602ff93c4e1d3ee71afb5
aadi-code <aadikumpavat@gmail.com>
2025-11-21 15:21:29 +0530

    Design changes and also the logic

--- script.js
+++ script.js
@@ -23,6 +23,9 @@
 
 const allElem = document.querySelectorAll("*");
 const bgChnage = document.querySelector("button");
+const nav = document.querySelector("nav");
+const logo = document.querySelector(".logo");
+const footer = document.querySelector("footer");
 
 // const kolors = ["red", "green", "blue", "pink", "black", "white", "yellow", "gold", "purple", "gray"];
 const cssColors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
@@ -86,8 +89,14 @@ bgChnage.addEventListener("click", () => {
 
     if(luminance < 128){
         document.body.style.color = "white";
+        nav.style.borderColor = "white";
+        logo.style.borderColor = "white";
+        footer.style.borderColor = "white";
     }else{
         document.body.style.color = "black";
+        nav.style.borderColor = "black";
+        logo.style.borderColor = "black";
+        footer.style.borderColor = "black";
     }
 
     document.body.style.backgroundColor = index;
